0feee71803a6ec50eb4250c304282e9ed39ea6dd
timur-malikov_visier <timur.malikov@visier.com>
2024-08-16 18:32:34 -0700

    [VAN-124119] Simplified code

--- time-machine/main.py
+++ time-machine/main.py
@@ -2,22 +2,17 @@ import argparse
 import logging
 import os
 import sys
-from datetime import date
-from enum import Enum
 
 from dotenv import load_dotenv
-from visier.sdk.api.data_out import Configuration
-
-from data_downloader import DataDownloader
-from data_uploader import DataUploader
-
-
-class Command(Enum):
-    DOWNLOAD = 'download'
-    UPLOAD = 'upload'
+from visier.sdk.api.data_in import DataUploadApi
+from visier.sdk.api.data_out import DataQueryApi, Configuration, ApiClient, AggregationQueryExecutionDTO
 
 
 def setup_logger() -> logging.Logger:
+    # turning off info logs for urllib3
+    urllib3_logger = logging.getLogger("urllib3")
+    urllib3_logger.setLevel(logging.WARNING)
+
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
@@ -30,6 +25,7 @@ def setup_logger() -> logging.Logger:
     console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
+
     return logger
 
 
@@ -37,6 +33,7 @@ logger = setup_logger()
 
 
 def load_api_configuration() -> Configuration:
+    logger.info("Loading API configuration.")  # Reduced message
     config = Configuration(
         host=os.getenv('VISIER_HOST'),
         api_key=os.getenv('VISIER_APIKEY'),
@@ -46,46 +43,85 @@ def load_api_configuration() -> Configuration:
         client_secret=os.getenv('VISIER_CLIENT_SECRET'),
         redirect_uri=os.getenv('VISIER_REDIRECT_URI'),
         vanity=os.getenv('VISIER_VANITY'))
-
     return config
 
 
 def parse_args():
-    logger.info("Parsing command line arguments: %s", " ".join(sys.argv))
-
+    logger.info(f"Parsing command line arguments: {sys.argv[1:]}.")  # Add arguments to log
     parser = argparse.ArgumentParser(description="Time Machine sample.")
-    parser.add_argument('-c', '--command', type=Command, required=True,
-                        help="Command type:\n"
-                             "download: download data using aggregate query definition.\n"
-                             "upload: upload data to overlay.")
-
-    parser.add_argument('-q', '--query_file_path', type=str,
+    parser.add_argument('-q', '--query_file_path', type=str, required=True,
                         help="File path to aggregate query definition.")
-    parser.add_argument('-s', '--storage_path', type=str,
-                        help="Storage path to save downloaded data files.")
-    parser.add_argument('-u', '--upload_file_path', type=str,
-                        help="File path to csv data to upload using Date Upload API.")
-    parser.add_argument('-d', '--date', type=str, default=date.today().strftime('%Y-%m-%d'),
-                        help="Date to get snapshot in aggregate query (YYYY-MM-DD format). "
-                             "Defaults to today's date if not set.")
-
     parsed_args = parser.parse_args()
-    logger.info("Arguments parsed. %s",
-                ", ".join(f"{arg}: {value}" for arg, value in vars(parsed_args).items()))
     return parsed_args
 
 
-def main():
-    args = parse_args()
-    load_dotenv(override=True)
-    api_config = load_api_configuration()
+def download_data(config: Configuration,
+                  query_file_path: str,
+                  data_file_path: str):
+    """
+    Downloads metrics data and saves it to a file.
+    Args:
+        config: The API configuration object.
+        query_file_path: The file path to the aggregate query definition.
+        data_file_path: The file path to save the downloaded data.
+    """
+    with open(query_file_path) as f:
+        headcount_json = f.read()
+    query_dto = AggregationQueryExecutionDTO.from_json(headcount_json)
 
-    if args.command == Command.DOWNLOAD:
-        downloader = DataDownloader(args.storage_path, api_config)
-        downloader.download_data(args.query_file_path, args.date)
-    elif args.command == Command.UPLOAD:
-        uploader = DataUploader(api_config)
-        uploader.upload_data(args.upload_file_path)
+    api_client = ApiClient(config, 'Accept', 'text/csv')
+    query_api = DataQueryApi(api_client)
+
+    logger.info(f"Executing query: {query_file_path}")  # Combined messages
+    response = query_api.aggregate_without_preload_content(query_dto,
+                                                           _headers={'Accept': 'text/csv'})
+
+    with open(data_file_path, mode='w') as f:
+        f.write(response.data.decode())
+    logger.info(f"Data saved to: {data_file_path}")  # Combined messages
+
+
+def upload_data(config: Configuration, data_file_path: str):
+    """
+    Uploads data using the Data API.
+    Args:
+        config: The API configuration object.
+        data_file_path: The file path to the data to be uploaded.
+    """
+    api_client = ApiClient(config, 'Content-Type', 'application/octet-stream')
+    upload_api = DataUploadApi(api_client)
+
+    with open(data_file_path, 'rb') as f:
+        data = f.read()
+
+    filename = os.path.basename(data_file_path)
+    logger.info(f"Uploading data: {filename}")  # Kept for visibility
+    upload_api.v1_data_upload_files_filename_put(filename=filename, body=data)
+
+
+def main():
+    try:
+        logger.info("Application started.")
+        args = parse_args()
+        load_dotenv(override=True)
+        api_config = load_api_configuration()
+
+        file_name, _ = os.path.splitext(os.path.basename(args.query_file_path))
+        storage_path = os.getenv('STORAGE_PATH', default='tmp_storage')
+        temp_data_file = os.path.join(storage_path, f'{file_name}.csv')
+
+        download_data(api_config, args.query_file_path, temp_data_file)
+
+        if os.getenv('UPLOAD_DATA'):
+            upload_data(api_config, temp_data_file)
+
+        if not os.getenv('KEEP_TEMP'):
+            os.remove(temp_data_file)
+            logger.info(f"Temporary file removed.")  # Added for clarity
+
+        logger.info("Application completed.")
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {e}")
 
 
 if __name__ == '__main__':
